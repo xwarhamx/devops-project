e0b429fd7e300f3c667724ddf817b497768cd1f6
Alex <aleksandrmar87@gmail.com>
2026-04-28 11:59:05 +0300

    DevOps project

---
+++ README.md
@@ -0,0 +1,13 @@
+# DevOps Project
+
+## Stack
+- Docker
+- Nginx
+- Flask
+- PostgreSQL
+
+## Run
+docker-compose up --build
+
+## Architecture
+nginx → app → db
---
+++ app/app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+	database=os.getenv("DB_NAME"),
+	user=os.getenv("DB_USER"),
+	password=os.getenv("DB_PASSWORD")
+    )
+    return conn
+
+@app.route("/")
+def home():
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("SELECT version();")
+        db_version = cur.fetchone()
+        cur.close()
+        conn.close()
+        return f"Hello DevOps! DB: {db_version}"
+    except Exception as e:
+        return f"DB connection error: {e}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
